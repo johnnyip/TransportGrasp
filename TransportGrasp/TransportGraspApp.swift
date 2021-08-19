5796a92e975c40ba2d03bf4a5d7fffe37f3f4119
unclechuen <33083306+unclechuen@users.noreply.github.com>
2021-08-19 20:02:16 +0800

    Initial Commit

---
+++ TransportGrasp/TransportGraspApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransportGraspApp.swift
+//  TransportGrasp
+//
+//  Created by Johnny on 19/8/2021.
+//
+
+import SwiftUI
+
+@main
+struct TransportGraspApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
